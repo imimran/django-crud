5d14d640125cb99b003db017056010d1428916c1
imimran <engr.aih@gmail.com>
2021-04-26 15:49:41 +0600

    Add views & templates

---
+++ crud/users/urls.py
@@ -0,0 +1,8 @@
+
+from django.urls import path, include
+from .  import views
+
+urlpatterns = [
+    path('', views.user_form),
+    path('list/', views.user_list),
+]
--- crud/users/views.py
+++ crud/users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def user_list(request):
+    return render(request, 'users/list.html')
+
+def user_form(request):
+     return render(request, 'users/form.html')
+
+def user_delete(request):
+     return 
