32def9b5722cf4a022f678a1c236655f57f197a8
imimran <engr.aih@gmail.com>
2021-04-26 14:42:40 +0600

    Add User & Posiition Model

--- crud/users/models.py
+++ crud/users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Position(models.Model):
+    title = models.CharField(max_length=100)
+
+
+class User(models.Model):
+    fullname = models.CharField(max_length=100)
+    adddress = models.CharField(max_length=100)
+    mobile = models.CharField(max_length=11)
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
