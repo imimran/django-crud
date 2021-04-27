288e992321fc10811dc2e4f17a44eecd948e4ecc
imimran <engr.aih@gmail.com>
2021-04-27 11:03:55 +0600

    Add User Done

--- crud/users/views.py
+++ crud/users/views.py
@@ -1,14 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .form import UserForm
 
 # Create your views here.
 
+
 def user_list(request):
     return render(request, 'users/list.html')
 
+
 def user_form(request):
-    form = UserForm()
-    return render(request, 'users/form.html', {'form': form})
+    if request.method == 'GET':
+        form = UserForm()
+        return render(request, 'users/form.html', {'form': form})
+
+    else:
+        form = UserForm(request.POST)
+        if form.is_valid() :
+            form.save()
+        return redirect('/user/list.html')
+
 
 def user_delete(request):
-     return 
+    return
